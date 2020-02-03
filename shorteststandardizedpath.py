982a9677c2b9998d404028be1b869da9029253fb
Rick <buddgarcia@gmail.com>
2020-02-02 22:31:45 -0800

    wrote a solution that does not utilize the regex

--- shorteststandardizedpath.py
+++ shorteststandardizedpath.py
@@ -2,7 +2,7 @@
 #For example, given "/usr/bin/../bin/./scripts/../", return "/usr/bin/".
 import re
 
-path = "/usr/bin/../bin/./scripts/../"
+path = "/usr/bin/../bin/./scripts/../test/././../thereshouldbenotest"
 
 #given the path is a string we need to sperate the path into a list of seperate strings split on the '/'
 
@@ -26,3 +26,30 @@ for str in newpathlist:
 
 print(shortestpath)
 
+#non-regex method
+# substr = ''
+# pathlist=[]
+# for ind, elm in enumerate(path):
+#   if elm == '/':
+#     if substr != '':
+#       pathlist.append(substr)
+#       substr = ''
+#   elif ind == len(path)-1:
+#     substr += elm
+#     pathlist.append(substr)
+#   else:
+#     substr += elm
+
+# newpathlist = []
+# for str in pathlist:
+#   if str == '..':
+#     newpathlist.pop()
+#   elif str != '.':
+#     newpathlist.append(str)
+
+# shortestpath ='/'
+
+# for str in newpathlist:
+#   shortestpath += str+'/'
+
+# print(shortestpath)
