0e900957bc240da1dc62597f8e183d93f49d0cf3
Rick <buddgarcia@gmail.com>
2020-02-02 18:50:18 -0800

    wrote the code to solve the problem

--- shorteststandardizedpath.py
+++ shorteststandardizedpath.py
@@ -1,18 +1,28 @@
 #Given an absolute pathname that may have . or .. as part of it, return the shortest standardized path.
 #For example, given "/usr/bin/../bin/./scripts/../", return "/usr/bin/".
-
+import re
 
 path = "/usr/bin/../bin/./scripts/../"
 
-#given the path is a string we need to sperate the path into a list of seperate strings that start with a / and end before the next /
+#given the path is a string we need to sperate the path into a list of seperate strings split on the '/'
 
-pathlist = []
+
+pathlist = re.sub(r'/', ' ', path).split()
 
 #we then need sort through the list accounting for removals ".." and additions "." to the path. For example ['/usr', '/bin', '../'] will become ['/usr,'] and ['/usr', '/bin', '/.', '/scripts'] will become ['/usr', '/bin', '/scripts']
 newpathlist = []
+for str in pathlist:
+  if str == '..':
+    newpathlist.pop()
+  elif str != '.':
+    newpathlist.append(str)
+
 
 #we then will take the sorted list and create a new string which will be the shortest path
+shortestpath ='/'
 
+for str in newpathlist:
+  shortestpath += str+'/'
 
-
+print(shortestpath)
 
